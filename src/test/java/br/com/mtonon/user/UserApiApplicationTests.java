3ccdbb12f2f35429989a6cb60b3ebd71d3af051a
mbettcher <mtonon.pmg@gmail.com>
2023-04-24 11:21:46 -0300

    Cria o projeto REST API user-api

---
+++ src/test/java/br/com/mtonon/user/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mtonon.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
